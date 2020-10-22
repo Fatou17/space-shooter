536fe400aef18e5616ad17b0c6a66ec3030745d7
Fatou17 <72991948+Fatou17@users.noreply.github.com>
2020-10-22 13:17:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let spaceship = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 3 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 3 . . . . . . . 
+    . . . . . . 8 8 1 a . . . . . . 
+    . . . . . . 8 3 1 a . . . . . . 
+    . . . . . c c c a a a . . . . . 
+    . . . . 8 8 3 3 3 1 a a . . . . 
+    . . 8 f f f c c a a f f a a . . 
+    . 8 8 8 8 a a 3 3 3 3 1 3 a a . 
+    8 8 8 8 8 8 a a 3 3 3 1 3 3 a a 
+    8 8 8 8 8 8 a a 3 3 3 3 1 3 a a 
+    `, SpriteKind.Player)
+spaceship.setPosition(80, 100)
+spaceship.setFlag(SpriteFlag.StayInScreen, true)
+game.onUpdate(function () {
+    spaceship.x += controller.dx()
+})
